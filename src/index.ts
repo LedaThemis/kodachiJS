65c9240c1fe3d8922a82394954b2fa2aa6caba95
Leda <ledathemis@protonmail.com>
2022-07-24 14:21:09 +0000

    feat: add dynamic commands loading/executing

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,59 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import {
+    Client,
+    Collection,
+    GatewayIntentBits,
+    SlashCommandBuilder,
+} from 'discord.js';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
-const client = new Client({
+interface CommandType {
+    data: SlashCommandBuilder;
+    execute: Function;
+}
+interface ExtendedClient extends Client {
+    commands?: Collection<string, CommandType>;
+}
+
+const client: ExtendedClient = new Client({
     intents: [GatewayIntentBits.Guilds],
 });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command: CommandType = require(filePath);
+
+    client.commands.set(command.data.name, command);
+}
 
 client.on('ready', () => {
     console.log('Ready!');
 });
 
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isChatInputCommand()) {
-        return;
-    }
+    if (!interaction.isChatInputCommand()) return;
 
-    const { commandName, options } = interaction;
+    const command = client.commands?.get(interaction.commandName);
 
-    if (commandName === 'ping') {
-        interaction.reply('Pong!');
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
     }
 });
 
