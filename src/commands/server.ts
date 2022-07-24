d12b79ebc9ceb79cce82d367b086171043babcac
Leda <ledathemis@protonmail.com>
2022-07-24 14:22:09 +0000

    feat: add server command

---
+++ src/commands/server.ts
@@ -0,0 +1,12 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Replies with server details'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.reply(
+            `Server name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`,
+        );
+    },
+};
