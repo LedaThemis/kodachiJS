7fada4235a94eee03b41770520024616eef5acd7
Leda <ledathemis@protonmail.com>
2022-07-25 19:29:21 +0000

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,10 @@ const client: ExtendedClient = new Client({
 mongoose.connect(process.env.MONGODB_URI!);
 const db = mongoose.connection;
 db.on('connected', () => console.log('INFO | Succesfully connected to DB'));
-db.on('error', console.error.bind(console, 'ERROR | MongoDB connection error:'));
+db.on(
+    'error',
+    console.error.bind(console, 'ERROR | MongoDB connection error:'),
+);
 
 // COMMANDS
 client.commands = new Collection();
