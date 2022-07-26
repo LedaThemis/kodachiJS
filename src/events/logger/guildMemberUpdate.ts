5a87a6f14f646ec0b96df5d59e5983185c080290
Leda <ledathemis@protonmail.com>
2022-07-26 12:36:18 +0000

    feat: add nickname logger

---
+++ src/events/logger/guildMemberUpdate.ts
@@ -0,0 +1,33 @@
+import { GuildMember, TextChannel } from 'discord.js';
+
+import config from '../../../config';
+import { ExtendedClient } from '../../interfaces/Client';
+
+module.exports = {
+    name: 'guildMemberUpdate',
+    async execute(
+        oldMember: GuildMember,
+        newMember: GuildMember,
+        client: ExtendedClient,
+    ) {
+        if (oldMember.nickname !== newMember.nickname) {
+            const nicknameChannel = (await client.channels.fetch(
+                config.logger.channels.nickname,
+            )) as TextChannel | null;
+
+            if (oldMember.nickname && newMember.nickname) {
+                nicknameChannel?.send(
+                    `<@${oldMember.id}> changed nickname from **${oldMember.nickname}** to **${newMember.nickname}** in **${oldMember.guild.name}**`,
+                );
+            } else if (!oldMember.nickname && newMember.nickname) {
+                nicknameChannel?.send(
+                    `<@${oldMember.id}> set nickname to **${newMember.nickname}** in **${oldMember.guild.name}**`,
+                );
+            } else {
+                nicknameChannel?.send(
+                    `<@${oldMember.id}> reset nickname in **${oldMember.guild.name}**`,
+                );
+            }
+        }
+    },
+};
