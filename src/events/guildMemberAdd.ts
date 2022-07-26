be42940e9ebff1f0c1d97b9ec4901e041bf40f65
Leda <ledathemis@protonmail.com>
2022-07-26 07:40:49 +0000

    feat: implement welcome event

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,10 +1,34 @@
-import { GuildMember } from 'discord.js';
+import { GuildMember, TextChannel } from 'discord.js';
 
+import config from '../../config';
 import { ExtendedClient } from '../interfaces/Client';
 
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member: GuildMember, client: ExtendedClient) {
         console.log(`${member.displayName} joined to ${member.guild.name}`);
+        const guildConfig = config.welcome.guilds[member.guild.id];
+
+        const welcomeChannel = client.channels.cache.get(
+            guildConfig.channel,
+        ) as TextChannel | undefined;
+
+        const processMessage = (message: string) => {
+            const userMention = `<@${member.id}>`;
+            return message.replace(/<@USER_MENTION>/g, userMention);
+        };
+
+        const processedMessage = processMessage(guildConfig.message);
+
+        if (guildConfig.attachment_url) {
+            welcomeChannel?.send({
+                content: processedMessage,
+                files: [guildConfig.attachment_url],
+            });
+        } else {
+            welcomeChannel?.send({
+                content: processedMessage,
+            });
+        }
     },
 };
